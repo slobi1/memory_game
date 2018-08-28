a2a9e5e0717448ed33bdd283cb9991a42da1d9e0
Slobodan Djordjevic <sdjordjevic@razor.rs>
2018-08-28 21:42:19 +0200

    Remove log

--- src/reducers/HighScoreReducer.js
+++ src/reducers/HighScoreReducer.js
@@ -3,7 +3,6 @@ import * as actions from "../constants/actionTypes";
 export default function HighScoreReducer(state = {}, action) {
   switch (action.type) {
     case actions.FETCH_SCORES: {
-      console.log('hello');
       return action.payload;
     }
 
